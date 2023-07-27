bafe18ce27066ab075bc4ad4f43008e601364d5c
Dara <ajaoemmanuel01@gmail.com>
2023-07-27 14:26:45 +0100

    progress

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function run() {
   let htmlCode = document.querySelector("#html-code").value;
-  let cssCode = <style> document.querySelector("#css-code").value </style>;
+  let cssCode = "<style>" + document.querySelector("#css-code").value + "</style>";
   let jsCode = document.querySelector("#js-code").value;
   let output = document.querySelector("#output");
-  //console.log(htmlCode, cssCode, jsCode, output);
-  output.contentDocument.body.innerHTML = htmlCode.cssCode;
+  // console.log(htmlCode, cssCode, jsCode, output);
+  output.contentDocument.body.innerHTML = htmlCode + cssCode;
+  output.contentWindow.eval(jsCode);
 }
+document.querySelector("#html-code").addEventListener("keyup", run);
+document.querySelector("#css-code").addEventListener("keyup", run);
+document.querySelector("#js-code").addEventListener("keyup", run);
