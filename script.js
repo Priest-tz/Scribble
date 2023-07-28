30b1ceee4e48915740f5abc48d077e827c439835
Dara <ajaoemmanuel01@gmail.com>
2023-07-28 04:52:47 +0100

    progress

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+"use strict";
+
 function run() {
   let htmlCode = document.querySelector("#html-code").value;
   let cssCode = "<style>" + document.querySelector("#css-code").value + "</style>";
   let jsCode = document.querySelector("#js-code").value;
   let output = document.querySelector("#output");
-  // console.log(htmlCode, cssCode, jsCode, output);
+  
   output.contentDocument.body.innerHTML = htmlCode + cssCode;
   output.contentWindow.eval(jsCode);
 }
+
 document.querySelector("#html-code").addEventListener("keyup", run);
 document.querySelector("#css-code").addEventListener("keyup", run);
 document.querySelector("#js-code").addEventListener("keyup", run);
+
+// Function to show the loader
+function showLoader() {
+  document.getElementById("loader").style.display = "block";
+}
+
+// Function to hide the loader
+function hideLoader() {
+  document.getElementById("loader").style.display = "none";
+}
